d38fe90c1eb9aa3b67a3662ab87cfe345ee986a8
cav-ipb <vcardent@ipb-halle.de>
2025-09-01 12:06:30 -0400

    feature: move docker configuration to project root

--- sources/core/setup.sh
+++
@@ -1,7 +0,0 @@
-mysql < /scripts/01_create.sql --user=root --password=${MYSQL_ROOT_PASSWORD}
-mysql < /scripts/02_load.sql --user=root --password=${MYSQL_ROOT_PASSWORD}
-mysql < /scripts/03_primary-keys.sql --user=root --password=${MYSQL_ROOT_PASSWORD}
-mysql < /scripts/04_foreign-keys.sql --user=root --password=${MYSQL_ROOT_PASSWORD}
-
-
-
